1a1fb7da4c42ba6c8c455defe0cee833364f2412
Elizabeth Harper <foxcapade@gmail.com>
2020-06-02 17:26:18 -0400

    version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ apply(from = "dependencies.gradle.kts")
 
 // Project settings
 group   = "org.veupathdb.lib"
-version = "1.2.0"
+version = "1.2.1"
 
 // Additional settings
 val moduleName = "epvb.lib.container.jaxrs.core"
