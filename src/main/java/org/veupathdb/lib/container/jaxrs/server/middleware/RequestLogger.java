76bc69b451b3bce729cf19ccb496c19b1f0b4d70
Ryan Doherty <rdoherty@upenn.edu>
2021-11-18 09:33:46 -0500

    Reduce metrics request logging to trace to avoid log noise; increase general log level to debug since framework is relatively mature

--- src/main/java/org/veupathdb/lib/container/jaxrs/server/middleware/RequestLogger.java
+++ src/main/java/org/veupathdb/lib/container/jaxrs/server/middleware/RequestLogger.java
@@ -1,15 +1,16 @@
 package org.veupathdb.lib.container.jaxrs.server.middleware;
 
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.util.Supplier;
-
+import java.util.function.Consumer;
 import javax.annotation.Priority;
-import javax.ws.rs.container.*;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.Response.Status.Family;
 import javax.ws.rs.ext.Provider;
-
-import java.util.function.Consumer;
-
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.util.Supplier;
 import org.veupathdb.lib.container.jaxrs.providers.LogProvider;
 
 import static java.lang.String.format;
@@ -19,26 +20,25 @@ import static java.lang.String.format;
 @PreMatching
 public class RequestLogger implements ContainerRequestFilter, ContainerResponseFilter {
   private static final String
-    START_FORMAT = "Request start: {} {}",
+    START_FORMAT = "Request start: %s %s",
     END_FORMAT   = "Request end: %s %s %d";
 
   private final Logger LOG = LogProvider.logger(RequestLogger.class);
 
   @Override
   public void filter(ContainerRequestContext req) {
-    LOG.debug(START_FORMAT, req.getMethod(), "/" + req.getUriInfo().getPath());
+    String path = req.getUriInfo().getPath();
+    // use trace for metrics requests to avoid log congestion
+    Consumer<Supplier<?>> fn = path.equals("metrics") ? LOG::trace : LOG::debug;
+    fn.accept(() -> format(START_FORMAT, req.getMethod(), "/" + path));
   }
 
   @Override
   public void filter(ContainerRequestContext req, ContainerResponseContext res) {
-    final Consumer<Supplier<?>> fn;
-    if (res.getStatusInfo().getFamily() == Family.SERVER_ERROR) {
-      fn = LOG::warn;
-    } else {
-      fn = LOG::debug;
-    }
-
-    fn.accept(() -> format(END_FORMAT, req.getMethod(),
-      "/" + req.getUriInfo().getPath(), res.getStatus()));
+    boolean isError = res.getStatusInfo().getFamily() == Family.SERVER_ERROR;
+    String path = req.getUriInfo().getPath();
+    // use trace for metrics requests to avoid log congestion; warn for errors
+    Consumer<Supplier<?>> fn = isError ? LOG::warn : path.equals("metrics") ? LOG::trace : LOG::debug;
+    fn.accept(() -> format(END_FORMAT, req.getMethod(), "/" + path, res.getStatus()));
   }
 }
