3be91316d5d05e4c974febf085307f586b32b576
Elizabeth <foxcapade@gmail.com>
2022-02-14 11:26:43 -0500

    b

--- src/main/java/org/veupathdb/lib/container/jaxrs/errors/InitFailedException.java
+++ src/main/java/org/veupathdb/lib/container/jaxrs/errors/InitFailedException.java
@@ -1,6 +1,6 @@
 package org.veupathdb.lib.container.jaxrs.errors;
 
-public class InitFailedException extends RuntimeException{
+public class InitFailedException extends RuntimeException {
   private static final String message = "Server initialization failed.";
 
   public InitFailedException(Throwable cause) {
