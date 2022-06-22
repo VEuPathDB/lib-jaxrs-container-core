647f9651f458e1820b9ba0762f4b628ccd9b7c6d
Elizabeth <foxcapade@gmail.com>
2022-06-22 13:00:28 -0400

    updated to use TwoTuple

--- src/main/java/org/veupathdb/lib/container/jaxrs/providers/UserProvider.java
+++ src/main/java/org/veupathdb/lib/container/jaxrs/providers/UserProvider.java
@@ -3,8 +3,8 @@ package org.veupathdb.lib.container.jaxrs.providers;
 import java.util.Objects;
 import java.util.Optional;
 import org.glassfish.jersey.server.ContainerRequest;
+import org.gusdb.fgputil.Tuples.TwoTuple;
 import org.veupathdb.lib.container.jaxrs.Globals;
-import org.veupathdb.lib.container.jaxrs.model.RequestAuth;
 import org.veupathdb.lib.container.jaxrs.model.User;
 import org.veupathdb.lib.container.jaxrs.server.middleware.AuthFilter;
 import org.veupathdb.lib.container.jaxrs.utils.RequestKeys;
@@ -17,7 +17,7 @@ public class UserProvider {
       .map(User.class::cast);
   }
 
-  public static Optional<RequestAuth> getSubmittedAuth(ContainerRequest req) {
+  public static Optional<TwoTuple<String, String>> getSubmittedAuth(ContainerRequest req) {
     String auth = Optional.ofNullable(Objects
       // caller must pass a non-null request
       .requireNonNull(req)
@@ -31,6 +31,6 @@ public class UserProvider {
 
     // convert value to Entry if present
     return Optional.ofNullable(auth)
-      .map(s -> new RequestAuth(RequestKeys.AUTH_HEADER, s));
+      .map(s -> new TwoTuple<>(RequestKeys.AUTH_HEADER, s));
   }
 }
