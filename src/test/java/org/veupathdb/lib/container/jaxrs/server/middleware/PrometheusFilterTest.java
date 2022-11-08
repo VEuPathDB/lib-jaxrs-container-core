c9c223b52d5501e3a83505a4dfd653bf484e2b7d
Dan Galdi <dmgaldi@upenn.edu>
2022-11-08 15:04:23 -0500

    Replace path params in duration label (#24)

--- src/test/java/org/veupathdb/lib/container/jaxrs/server/middleware/PrometheusFilterTest.java
+++ src/test/java/org/veupathdb/lib/container/jaxrs/server/middleware/PrometheusFilterTest.java
@@ -1,7 +1,10 @@
 package org.veupathdb.lib.container.jaxrs.server.middleware;
 
+import io.prometheus.client.Histogram;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.util.Supplier;
+import org.glassfish.jersey.server.ContainerRequest;
+import org.glassfish.jersey.server.ExtendedUriInfo;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -15,8 +18,10 @@ import jakarta.ws.rs.core.UriInfo;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
+import static org.veupathdb.lib.container.jaxrs.server.middleware.PrometheusFilter.MATCHED_URL_KEY;
+import static org.veupathdb.lib.container.jaxrs.server.middleware.PrometheusFilter.TIME_KEY;
 
-class RequestLoggerTest {
+class PrometheusFilterTest {
 
   @AfterEach
   void reset() throws Exception {
@@ -29,10 +34,10 @@ class RequestLoggerTest {
   void filter() throws Exception {
     var stat = mock(LogProvider.class);
     var log  = mock(Logger.class);
-    var req  = mock(ContainerRequestContext.class);
-    var uri  = mock(UriInfo.class);
+    var req  = mock(ContainerRequest.class);
+    var uri  = mock(ExtendedUriInfo.class);
 
-    when(stat.getLogger(RequestLogger.class)).thenReturn(log);
+    when(stat.getLogger(PrometheusFilter.class)).thenReturn(log);
     when(req.getMethod()).thenReturn("");
     when(req.getUriInfo()).thenReturn(uri);
     when(uri.getPath()).thenReturn("");
@@ -41,9 +46,9 @@ class RequestLoggerTest {
     i.setAccessible(true);
     i.set(null, stat);
 
-    new RequestLogger().filter(req);
+    new PrometheusFilter().filter(req);
 
-    verify(stat, times(1)).getLogger(RequestLogger.class);
+    verify(stat, times(1)).getLogger(PrometheusFilter.class);
     verify(log, times(1)).debug((Supplier<?>)any());
   }
 
@@ -54,21 +59,24 @@ class RequestLoggerTest {
     var req  = mock(ContainerRequestContext.class);
     var res  = mock(ContainerResponseContext.class);
     var uri  = mock(UriInfo.class);
+    var timer = mock(Histogram.Timer.class);
 
-    when(stat.getLogger(RequestLogger.class)).thenReturn(log);
+    when(stat.getLogger(PrometheusFilter.class)).thenReturn(log);
     when(req.getMethod()).thenReturn("");
     when(req.getUriInfo()).thenReturn(uri);
     when(uri.getPath()).thenReturn("");
     when(res.getStatusInfo()).thenReturn(Status.OK);
     when(res.getStatus()).thenReturn(0);
+    when(req.getProperty(TIME_KEY)).thenReturn(timer);
+    when(req.getProperty(MATCHED_URL_KEY)).thenReturn("url");
 
     var i = LogProvider.class.getDeclaredField("instance");
     i.setAccessible(true);
     i.set(null, stat);
 
-    new RequestLogger().filter(req, res);
+    new PrometheusFilter().filter(req, res);
 
-    verify(stat, times(1)).getLogger(RequestLogger.class);
+    verify(stat, times(1)).getLogger(PrometheusFilter.class);
     verify(log, times(1)).debug((Supplier<?>) any());
   }
 
@@ -81,23 +89,26 @@ class RequestLoggerTest {
     var req  = mock(ContainerRequestContext.class);
     var res  = mock(ContainerResponseContext.class);
     var uri  = mock(UriInfo.class);
+    var timer = mock(Histogram.Timer.class);
 
-    when(stat.getLogger(RequestLogger.class)).thenReturn(log);
+    when(stat.getLogger(PrometheusFilter.class)).thenReturn(log);
     when(req.getMethod()).thenReturn("");
     when(req.getUriInfo()).thenReturn(uri);
     when(uri.getPath()).thenReturn("");
     when(res.getStatusInfo()).thenReturn(Status.INTERNAL_SERVER_ERROR);
     when(res.getStatus()).thenReturn(0);
+    when(req.getProperty(TIME_KEY)).thenReturn(timer);
+    when(req.getProperty(MATCHED_URL_KEY)).thenReturn("url");
 
     var i = LogProvider.class.getDeclaredField("instance");
     i.setAccessible(true);
     i.set(null, stat);
 
-    new RequestLogger().filter(req, res);
+    new PrometheusFilter().filter(req, res);
 
     ArgumentCaptor<Supplier<?>> capt = ArgumentCaptor.forClass(Supplier.class);
 
-    verify(stat, times(1)).getLogger(RequestLogger.class);
+    verify(stat, times(1)).getLogger(PrometheusFilter.class);
     verify(log, times(1)).warn(capt.capture());
     assertTrue(capt.getValue().get() instanceof String);
   }
