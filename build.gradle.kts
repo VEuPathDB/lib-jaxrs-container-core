c4a7130a0be04c08c3d5349c28055d9200df81d4
Dan Galdi <dmgaldi@upenn.edu>
2022-11-23 14:49:19 -0500

    Added validation to authorization filter

--- build.gradle.kts
+++ build.gradle.kts
@@ -97,6 +97,7 @@ dependencies {
   // Unit Testing
   testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
   testImplementation("org.mockito:mockito-core:4.8.0")
+  testImplementation("org.mockito:mockito-junit-jupiter:4.8.0")
 }
 
 tasks.jar {
