59dc3e57e97aa60430fb9efb84e46ff39acf5f46
Dan Galdi <dmgaldi@upenn.edu>
2023-02-06 14:10:47 -0500

    Ensure request ID is maintained throughout entire request (#28)

--- src/main/java/org/veupathdb/lib/container/jaxrs/server/middleware/RequestIdFilter.java
+++ src/main/java/org/veupathdb/lib/container/jaxrs/server/middleware/RequestIdFilter.java
@@ -3,12 +3,15 @@ package org.veupathdb.lib.container.jaxrs.server.middleware;
 import com.devskiller.friendly_id.FriendlyId;
 import jakarta.annotation.Priority;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.container.PreMatching;
 import jakarta.ws.rs.ext.Provider;
+import jakarta.ws.rs.ext.WriterInterceptor;
+import jakarta.ws.rs.ext.WriterInterceptorContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
@@ -17,6 +20,8 @@ import org.veupathdb.lib.container.jaxrs.Globals;
 import org.veupathdb.lib.container.jaxrs.utils.RequestKeys;
 import org.veupathdb.lib.container.jaxrs.utils.logging.LoggingVars;
 
+import java.io.IOException;
+
 /**
  * Assigns a unique ID to each request for logging, error tracing purposes.
  */
@@ -24,7 +29,7 @@ import org.veupathdb.lib.container.jaxrs.utils.logging.LoggingVars;
 @Priority(0)
 @PreMatching
 public class RequestIdFilter
-implements ContainerRequestFilter, ContainerResponseFilter {
+implements ContainerRequestFilter, ContainerResponseFilter, WriterInterceptor {
 
   private static final Logger LOG = LogManager.getLogger(RequestIdFilter.class);
 
@@ -52,8 +57,28 @@ implements ContainerRequestFilter, ContainerResponseFilter {
   }
 
   @Override
+  public void aroundWriteTo(WriterInterceptorContext context) throws IOException, WebApplicationException {
+    LOG.trace("RequestIdFilter#aroundWriteTo(context)");
+    try {
+      // write the response
+      context.proceed();
+    }
+    finally {
+      removeContext();
+    }
+  }
+
+  @Override
   public void filter(ContainerRequestContext req, ContainerResponseContext res) {
     LOG.trace("RequestIdFilter#filter(req, res)");
+    // Remove request ID from thread context only if we have no response body.
+    // If we have a response body rely on aroundWriteTo to remove from thread context.
+    if (!res.hasEntity()) {
+      removeContext();
+    }
+  }
+
+  private void removeContext() {
     ThreadContext.remove(Globals.CONTEXT_ID);
     LoggingVars.clear();
   }
