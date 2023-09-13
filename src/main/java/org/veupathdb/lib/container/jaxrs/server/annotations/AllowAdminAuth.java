2f4f48d9e6783398c65a0e0f3cb11ff237e9abda
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2023-09-13 14:50:44 -0400

    update comment

--- src/main/java/org/veupathdb/lib/container/jaxrs/server/annotations/AllowAdminAuth.java
+++ src/main/java/org/veupathdb/lib/container/jaxrs/server/annotations/AllowAdminAuth.java
@@ -6,8 +6,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation that flags a resource as requiring a valid user auth token to
- * execute.
+ * Annotation that flags a resource as allowing authenticating requests via an
+ * admin token value rather than the standard authentication declared by using
+ * the annotation {@link Authenticated}.
+ * <p>
+ * This method is meant to be used in combination with the {@link Authenticated}
+ * annotation, and does nothing when used on its own.
  */
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
