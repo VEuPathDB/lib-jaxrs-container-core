4ffc28e65ab06042b832d3b1a361bb7c43d208c2
Ryan Doherty <tech@conical.org>
2024-02-07 23:08:14 -0500

    upgrade oauth

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
   implementation("org.gusdb:fgputil-accountdb:${fgputil}")
 
   // OAuth Client
-  implementation("org.gusdb:oauth2-client:0.7.6")
+  implementation("org.gusdb:oauth2-client:0.7.7")
 
   // Oracle
   runtimeOnly("com.oracle.database.jdbc:ojdbc8:21.9.0.0")
