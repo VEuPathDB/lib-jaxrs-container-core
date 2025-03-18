9ac690e7c2f902997751f17617e6cca6a2166a83
Ryan Doherty <tech@conical.org>
2025-03-18 18:11:25 -0400

    Add compatible junit launcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -99,7 +99,8 @@ dependencies {
 
   // Unit Testing
   testImplementation(platform("org.junit:junit-bom:5.12.0"))
-  testImplementation("org.junit.jupiter:junit-jupiter")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.12.0")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.12.0")
   testImplementation("org.mockito:mockito-core:5.15.2")
   testImplementation("org.mockito:mockito-junit-jupiter:5.15.2")
 }
